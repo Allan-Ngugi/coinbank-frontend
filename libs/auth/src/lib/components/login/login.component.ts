148381d94c669bf39a079a845e1f70b6922b2f23
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-07-17 00:38:07 +0300

    🚑 Major  Inputs

--- libs/auth/src/lib/components/login/login.component.ts
+++ libs/auth/src/lib/components/login/login.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'coinbank-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.credentials).subscribe(
+      (response) => {
+        console.log(`Welcome, ${response.username}!`);
+      },
+      (error) => {
+        console.error(
+          'An error occurred during login. Please try again.'
+        );
+      }
+    );
+  }
+}
