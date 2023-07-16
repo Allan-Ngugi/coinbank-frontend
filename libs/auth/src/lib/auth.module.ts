8964818a5943896dceb844fa3413c76559979e6f
lilmithi <gathitumithi9000@gmail.com>
2023-07-16 10:42:44 +0300

    Major changes

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginSkeletonComponent } from './components/login-skeleton/login-skeleton.component';
 
 @NgModule({
   imports: [CommonModule, AuthRoutingModule],
-  declarations: [SignupComponent, LoginComponent],
+  declarations: [SignupComponent, LoginComponent, LoginSkeletonComponent],
 })
 export class AuthModule {}
