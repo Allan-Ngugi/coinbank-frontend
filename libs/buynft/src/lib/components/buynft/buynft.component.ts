3761bf1b09da6583ea87ed7066316e25d13b590b
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-07-20 14:33:40 +0300

    ✨ modal for buynft purchase

--- libs/buynft/src/lib/components/buynft/buynft.component.ts
+++ libs/buynft/src/lib/components/buynft/buynft.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
-// import { OverlayPanelModule } from 'primeng/overlaypanel';
+// buynft.component.ts
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { OverlayPanel } from 'primeng/overlaypanel';
 
 @Component({
   selector: 'coinbank-buynft',
   templateUrl: './buynft.component.html',
   styleUrls: ['./buynft.component.scss'],
 })
-export class BuynftComponent {}
+export class BuynftComponent implements AfterViewInit {
+  @ViewChild('op') op!: OverlayPanel | undefined; // Add union type with undefined
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    // The p-overlayPanel element is available here
+    // You can perform any initialization or checks related to the element
+  }
+
+  showOverlayPanel() {
+    this.op?.toggle(true); // Use safe navigation operator
+  }
+}
