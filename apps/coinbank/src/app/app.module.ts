71f15cc2defad84a4e4a19a5c7f8255ace82d820
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-07-20 14:33:07 +0300

    ✨ Initialized all requirements for primeng

--- apps/coinbank/src/app/app.module.ts
+++ apps/coinbank/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent,],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,AppRoutingModule,],
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/buynft/src/lib/buynft.module.ts
+++ libs/buynft/src/lib/buynft.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BuynftComponent } from './components/buynft/buynft.component';
 import { BuynftRoutingModule } from './components/buynft-routing.module';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
-  imports: [CommonModule, BuynftRoutingModule],
+  imports: [CommonModule, BuynftRoutingModule, OverlayPanelModule],
   declarations: [BuynftComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class BuynftModule {}
