544e6260cdb3c7b50ff92e517d37aeda4aecb4a5
lilmithi <gathitumithi9000@gmail.com>
2023-07-25 21:00:25 +0300

    Minor changes

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginSkeletonComponent } from './components/login-skeleton/login-skeleton.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FeaturesModule } from '@coinbank/features';
 
 @NgModule({
-  imports: [CommonModule, AuthRoutingModule, HttpClientModule, FeaturesModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    HttpClientModule,
+    FeaturesModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   declarations: [SignupComponent, LoginComponent, LoginSkeletonComponent],
 })
 export class AuthModule {}
--- libs/auth/src/lib/components/signup/signup.component.ts
+++ libs/auth/src/lib/components/signup/signup.component.ts
@@ -1,39 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'coinbank-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
-  user = {
-    name: '',
-    email: '',
-    password: '',
-  };
-
-  constructor(private authService: AuthService) {}
+export class SignupComponent implements OnInit {
+  formData!: FormGroup;
+  constructor(private authService: AuthService, private _fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.formData = this._fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+    });
+  }
 
   signUp() {
-    this.authService.signUp(this.user).subscribe(
-      (response: any) => {
-        console.log('Sign-up successful');
-      },
-      (error: any) => {
-        console.error('Sign-up failed');
-        if (error.status === 409) {
-          console.error(
-            'User with the provided email already exists. Please choose a different email.'
-          );
-        } else if (error.status === 422) {
-          console.error('Validation error. Please check the entered data.');
-        } else {
-          console.error(
-            'An error occurred during sign-up. Please try again later.'
-          );
+    if (this.formData.valid) {
+      this.authService.signUp(this.formData.value).subscribe(
+        (data) => {
+          console.log(data);
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log(error);
         }
-      }
-    );
+      );
+    } else {
+      console.log('Form input error!');
+    }
+    // this.authService.signUp(this.user).subscribe(
+    //   (response: any) => {
+    //     console.log('Sign-up successful');
+    //   },
+    //   (error: any) => {
+    //     console.error('Sign-up failed');
+    //     if (error.status === 409) {
+    //       console.error(
+    //         'User with the provided email already exists. Please choose a different email.'
+    //       );
+    //     } else if (error.status === 422) {
+    //       console.error('Validation error. Please check the entered data.');
+    //     } else {
+    //       console.error(
+    //         'An error occurred during sign-up. Please try again later.'
+    //       );
+    //     }
+    //   }
+    // );
   }
 }
