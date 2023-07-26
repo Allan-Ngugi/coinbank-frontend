dd3ae75758c552b19a2bdb65af199ab52d08b76e
Allan Ngugi <105485948+Allan-Ngugi@users.noreply.github.com>
2023-07-27 00:18:33 +0300

    Minor Changes

--- libs/auth/src/lib/components/login/login.component.ts
+++ libs/auth/src/lib/components/login/login.component.ts
@@ -1,29 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'coinbank-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-  credentials = {
-    email: '',
-    password: '',
-  };
-
-  constructor(private authService: AuthService) {}
+export class LoginComponent implements OnInit {
+  formData!: FormGroup;
+  constructor(private authService: AuthService, private _fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.formData = this._fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+    });
+  }
 
   login() {
-    this.authService.login(this.credentials).subscribe(
-      (response) => {
-        console.log(`Welcome, ${response.username}!`);
-      },
-      (error) => {
-        console.error(
-          'An error occurred during login. Please try again.'
-        );
-      }
-    );
+    if (this.formData.valid) {
+      this.authService.login(this.formData.value).subscribe(
+        (data) => {
+          console.log(data);
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      console.log('Form input error!');
+    }
   }
 }
